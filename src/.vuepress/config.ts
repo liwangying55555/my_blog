28ca50b53efadfe014452fb6e53d451ed4fbb70b
wangying <280796176@qq.com>
2023-12-01 17:08:28 +0800

    fix: 缓存

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,11 @@
-import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+import { defineUserConfig } from 'vuepress'
+import theme from './theme.js'
 
 export default defineUserConfig({
-  base: "/",
-
-  lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
-
+  base: '/',
+  lang: 'zh-CN',
+  title: '泛泛之谈，个人娱乐',
+  description: 'js,javascript,cp,bjyx,bjyxszd,html,css',
+  port: 8000,
   theme,
-
-  // Enable it with pwa
-  // shouldPrefetch: false,
-});
+})
