fcf10102fb095a0defc908f8a094a4756759a3c8
wangying <280796176@qq.com>
2023-12-05 17:06:02 +0800

    fix: 配置

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,4 +3,10 @@ import { sidebar } from 'vuepress-theme-hope'
 export default sidebar({
   '/Tool': 'structure',
   '/Moment': 'structure',
+  '/BackEnd': 'structure',
+  '/FrontEnd/HTML': 'structure',
+  '/FrontEnd/JavaScript': 'structure',
+  '/FrontEnd/CSS': 'structure',
+  '/Focus': 'structure',
+  '/About': 'structure',
 })
