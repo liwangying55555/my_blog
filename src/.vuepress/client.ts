ef4039f86568e82d3f85ef090d9402996332c9ac
wangying <280796176@qq.com>
2024-11-14 14:23:29 +0800

    add busuanzi 访问量统计

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,9 +1,18 @@
 import TravellingLink from './components/TravellingLink/index.vue'
 import { defineClientConfig } from '@vuepress/client'
 
+const addBusuanzi = () => {
+  const script = document.createElement('script')
+  script.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'
+  script.async = true
+  document.head.appendChild(script)
+}
+
 export default defineClientConfig({
   enhance: ({ app, router, siteData }) => {
     app.component('TravellingLink', TravellingLink)
   },
-  setup() {},
+  setup() {
+    addBusuanzi()
+  },
 })
