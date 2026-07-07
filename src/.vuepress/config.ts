5e281fd17908d7bd121fb1b370940d7d06cd8de8
wangying <280796176@qq.com>
2026-07-07 10:06:30 +0800

    fix: github pages 配置部署

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from './config/theme.js'
 import { configPlugins as plugins } from './config/plugins.js'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/my_blog/',
   lang: 'zh-CN',
   title: '泛泛之谈『个人娱乐』',
   description: 'js,javascript,cp,bjyx,bjyxszd,html,css',
